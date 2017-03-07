e99a4ea2b5ca55464cac286ebe7723e2faff2e7f
gapatprakash@gmail.com <gapatprakash@gmail.com>
2017-03-07 10:33:19 +0530

    Done

---
+++ Student.ashx.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Web;
+using System.Web.Script.Serialization;
+
+namespace WebServiceAutocomplete
+{
+    /// <summary>
+    /// Summary description for Student
+    /// </summary>
+    public class Student : IHttpHandler
+    {
+
+        public void ProcessRequest(HttpContext context)
+        {
+            string term = context.Request["term"] ?? "";
+            List<string> listStudentNames = new List<string>();
+
+            string cs = "Data Source=PRAKASH-G;Initial Catalog=Xoriant121;Integrated Security=True;Connect Timeout=60;Encrypt=False;TrustServerCertificate=True;ApplicationIntent=ReadWrite;MultiSubnetFailover=False";
+            using (SqlConnection con = new SqlConnection(cs))
+            {
+                SqlCommand cmd = new SqlCommand("spGetStudentNames", con);
+                cmd.CommandType = CommandType.StoredProcedure;
+                cmd.Parameters.AddWithValue("@term", term);
+                con.Open();
+                SqlDataReader rdr = cmd.ExecuteReader();
+                while (rdr.Read())
+                {
+                    listStudentNames.Add(rdr["Name"].ToString());
+                }
+            }
+
+            JavaScriptSerializer js = new JavaScriptSerializer();
+            context.Response.Write(js.Serialize(listStudentNames));
+        }
+
+        public bool IsReusable
+        {
+            get
+            {
+                return false;
+            }
+        }
+    }
+}
---
+++ WebForm1.aspx
@@ -0,0 +1,31 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="WebForm1.aspx.cs" Inherits="WebServiceAutocomplete.WebForm1" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+   <title></title>
+    <script src="jquery.js"></script>
+    <script src="jquery-ui.js"></script>
+    <link href="jquery-ui.css" rel="stylesheet" />
+
+
+    <script type="text/javascript">
+        $(document).ready(function () {
+            $('#txtName').autocomplete({
+                source: 'Student.ashx'
+            });
+        });
+    </script>
+
+
+
+</head>
+<body style="font-family: Arial">
+    <form id="form1" runat="server">
+        Student Name :
+        <asp:TextBox ID="txtName" runat="server">
+        </asp:TextBox>
+    </form>
+</body>
+</html>
